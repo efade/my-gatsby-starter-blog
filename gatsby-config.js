d6b34a4780eaf4f2b7f1500e2be69cb28010abd1
Fakhruddin Ahmad Darwis <fakhri.satu@gmail.com>
2020-09-20 11:01:17 +0700

    Added tracking ID

--- gatsby-config.js
+++ gatsby-config.js
@@ -108,7 +108,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // edit below
-        // trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-178427069-1`,
       },
     },
     {
